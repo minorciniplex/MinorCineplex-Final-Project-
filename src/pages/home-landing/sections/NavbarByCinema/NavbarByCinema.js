ebb309c392af6755ac029ecf3441af83b0641e4c
Sanya Bochoun <76147037+sanya-bochoun@users.noreply.github.com>
2025-05-06 12:21:27 +0700

    feat(home-landing) create link to login/register
    Created link to login and register/improve homeLanding

--- src/pages/home-landing/sections/NavbarByCinema/NavbarByCinema.js
+++ src/pages/home-landing/sections/NavbarByCinema/NavbarByCinema.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Button from "@/components/Button";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const NavbarByCinema = () => {
+     const router = useRouter();
+
+     const handleLogin = () => {
+          router.push("/auth/login");
+     };
+
+     const handleRegister = () => {
+          router.push("/auth/register");
+     };
+
      return (
           <header className="flex w-full h-20 items-center justify-between px-20 py-0 bg-[#00000033] border-b border-base-gray-100 backdrop-blur-[7.5px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(7.5px)_brightness(100%)] fixed top-0 z-50">
                <div className="relative w-[42px] h-12">
@@ -21,6 +32,7 @@ const NavbarByCinema = () => {
                     <Button
                          variant="ghost"
                          className="text-white hover:text-white/80"
+                         onClick={handleLogin}
                     >
                          Login
                     </Button>
@@ -28,6 +40,7 @@ const NavbarByCinema = () => {
                     <Button
                          variant="secondary"
                          className=""
+                         onClick={handleRegister}
                     >
                          Register
                     </Button>

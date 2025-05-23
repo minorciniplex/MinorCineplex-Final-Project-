4b5e3f7432eb267e45f115f15de1fc848a6dd4c2
ChaL <chaluntonvipusanapas@gmail.com>
2025-05-23 16:57:31 +0700

    feat: Enhance booking page layout with StepProgressBar and SeatLayout components

---
+++ src/components/Booking/SeatLayout.js
@@ -0,0 +1,9 @@
+export default function SeatLayout() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <h1>Screen</h1>
+      </div>
+    </>
+  );
+}

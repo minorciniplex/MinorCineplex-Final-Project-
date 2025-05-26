1b80191d51d66f137b50cd6a5ba5b6d7bec2954f
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-05-26 11:52:41 +0700

    feat: Implement booking and payment components with enhanced seat selection and pricing functionality

--- src/components/Booking/PaymentsCard.js
+++ src/components/Booking/PaymentsCard.js
@@ -2,10 +2,8 @@ import FmdGoodIcon from "@mui/icons-material/FmdGood";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
-import Image from "next/image";
-import { useRouter } from "next/router";
 
-export default function BookingCard({
+export default function PaymentsCard({
   time,
   screenNumber,
   cinemaName,
@@ -15,10 +13,21 @@ export default function BookingCard({
   language,
   date,
   seat,
-  price
+  price,
 }) {
-
-  const router = useRouter();
+  // แปลง genres และ language ถ้ามาเป็น string
+  let seatArr = [];
+  try {
+    seatArr =
+      typeof seat === "string"
+        ? JSON.parse(seat)
+        : Array.isArray(seat)
+        ? seat
+        : [];
+  } catch {
+    seatArr = [];
+  }
+  console.log(seatArr);
   let genreArr = [];
   try {
     genreArr =
@@ -61,46 +70,21 @@ export default function BookingCard({
     });
   }
 
-  const handleSumbit = () => {
-    // ตัวอย่างการ push ไปหน้าจองตั๋ว
-    const query = new URLSearchParams({
-      poster: poster,  
-      title: title,
-      time: time,
-      date: date,
-      screenNumber: screenNumber,
-      genres: JSON.stringify(genres), // แปลง object เป็น string
-      language: JSON.stringify(language),
-      cinemaName: cinemaName,
-      seat: JSON.stringify(seat),
-      price: price
-      
-    }).toString();
-
-    router.push(`/booking/seats/payment/payment?${query}`);
-  };
-
-
-
-  
-
   return (
     <>
       <div className="flex justify-center items-center min-h-screen">
-        <div className="flex flex-col items-center w-[375px] p-4 bg-[--base-gray-0] rounded-lg shadow-md">
-          <div className="flex items-center justify-center w-full rounded-t-lg">
-            <Image
-            src={poster}
-            alt="Movie Poster"
-            width={82}
-            height={120}
-            className="object-cover rounded-lg"
-          />
-              <div className="flex flex-col justify-center w-full rounded-t-lg">
+        <div className="flex flex-col items-center w-[375px]  p-4 bg-[#18192b] rounded-lg shadow-md">
+          <div className="flex items-center justify-center w-full h-16 rounded-t-lg">
+            <img
+              src={poster}
+              alt="Movie Poster"
+              className="w-[82px] h-[120px] object-cover rounded-lg mb-4"
+            />
+            <div className="flex-col w-full h-16 rounded-t-lg flex items-center justify-center">
               <h2 className="text-white text-2xl font-bold mb-3 text-center">
                 {title}
               </h2>
-              <div className="flex px-2 gap-2 ">
+              <div className="flex gap-2 ">
                 {genreArr?.map((genre, index) => (
                   <span
                     key={index}
@@ -110,67 +94,81 @@ export default function BookingCard({
                   </span>
                 ))}
                 {lang && (
-                  <span className="bg-[--base-gray-100] py-[4px] md:py-[6px] px-3 rounded text-xs md:text-sm text-[--base-gray-400] font-medium">
+                  <span className="bg-[--base-gray-100] py-[4px] md:py-[6px] px-3 rounded text-xs md:text-sm text-[#C8CEDD] font-medium">
                     {lang.toUpperCase()}
                   </span>
                 )}
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-2 w-full pt-6 text-[--base-gray-400] text-base">
+          <div className="flex flex-col gap-2 w-full pt-10 text-[--base-gray-300] text-base">
             <div className="flex items-center gap-2">
               <FmdGoodIcon
-                className="text-[--base-gray-200]"
+                className="text-[--base-gray-300]"
                 fontSize="small"
               />
               <span>{cinemaName}</span>
             </div>
             <div className="flex items-center gap-2">
               <CalendarMonthIcon
-                className="text-[--base-gray-200]"
+                className="text-[--base-gray-300]"
                 fontSize="small"
               />
               <span>{formatDate(date)}</span>
             </div>
             <div className="flex items-center gap-2">
               <AccessTimeIcon
-                className="text-[--base-gray-200]"
+                className="text-[--base-gray-300]"
                 fontSize="small"
               />
               <span>{time}</span>
             </div>
             <div className="flex items-center gap-2">
               <MeetingRoomIcon
-                className="text-[--base-gray-200]"
+                className="text-[--base-gray-300]"
                 fontSize="small"
               />
               <span>Hall {screenNumber}</span>
             </div>
-            {seat.length === 0 ? null : (
+            {seatArr.length === 0 ? null : (
               <>
+              <div className="flex flex-col justify-between items-center gap-2">
+                <p className="text-[--base-gray-300]  mt-4 w-full">
+                    Coupon Code
+                </p>
+                <input className="w-full bg-[#232B47] rounded px-3 py-2 text-sm placeholder-gray-400">
+                
+                </input>
+
+
+              </div>
                 <div className="flex flex-row justify-between items-center gap-2">
                   <p className="text-[--base-gray-300] text-center mt-4">
                     Selected Seat
                   </p>
                   <p className="text-[#FFFFFF] text-center mt-4">
-                    {seat.length > 0 ? seat.join(", ") : ""}
+                    {seatArr.length > 0 ? seatArr.join(", ") : ""}
                   </p>
                 </div>
                 <div className="flex flex-row justify-between items-center gap-2">
                   <p className="text-[--base-gray-300] text-center">
-                    Total
+                    Payment method
                   </p>
+                  <p className="text-[#FFFFFF] text-center ">Credit card</p>
+                </div>
+                <div className="flex flex-row justify-between items-center gap-2">
+                  <p className="text-[--base-gray-300] text-center">Coupon</p>
+                  <p className="text-red-500 text-center ">-THB50</p>
+                </div>
+                <div className="flex flex-row justify-between items-center gap-2">
+                  <p className="text-[--base-gray-300] text-center">Total</p>
                   <p className="text-[#FFFFFF] text-center ">
                     {price > 0 ? `THB${price}` : ""}
                   </p>
-             
                 </div>
-                     <button
-                    onClick={handleSumbit}
-                    className="bg-[#4E7BEE] text-white px-4 py-2 rounded-lg mt-4 hover:bg-[#5a8cd9] transition-colors"
-                  >
-                    Next
-                  </button>
+                <button className="bg-[#4E7BEE] text-white px-4 py-2 rounded-lg mt-4 hover:bg-[#5a8cd9] transition-colors">
+                  Next
+                </button>
               </>
             )}
           </div>
--- src/pages/booking/seats/[seat].js
+++ src/pages/booking/seats/[seat].js
@@ -2,14 +2,17 @@ import BookingCard from "@/components/Booking/BookingCard";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Navbar from "@/components/Navbar/Navbar";
+import BookingSeats from "@/components/Booking/BookingSeats";
 import StepProgressBar from "@/components/Booking/StepProgressBar";
-import SeatLayout from "@/components/Booking/SeatLayout";
+/* import SeatLayout from "@/components/Booking/SeatLayout"; */
 
 export default function Seats() {
   const router = useRouter();
   const { time, screenNumber, cinemaName, poster, title, date } = router.query;
   const [genres, setGenres] = useState(null);
   const [language, setLanguage] = useState(null);
+  const [bookingSeat, setBookingSeat] = useState([]);
+const [sumPrice, setSumPrice] = useState(0);
 
   useEffect(() => {
     if (router.isReady) {
@@ -20,23 +23,30 @@ export default function Seats() {
     }
   }, [router.isReady]);
 
+
+ 
+  
+
+
   return (
     <>
       <Navbar />
       <StepProgressBar />
-      <div className="flex flex-row sm:py-20 sm:px-[120px] gap-[102px]">
-        <SeatLayout />
-        <BookingCard
-          time={time}
-          cinemaName={cinemaName}
-          screenNumber={screenNumber}
-          poster={poster}
-          title={title}
-          genres={genres}
-          language={language}
-          date={date}
-        />
-      </div>
+      <BookingSeats setBookingSeat={setBookingSeat} setSumPrice={setSumPrice} />
+      <BookingCard
+        time={time}
+        cinemaName={cinemaName}
+        screenNumber={screenNumber}
+        poster={poster}
+        title={title}
+        genres={genres}
+        language={language}
+        date={date}
+        seat={bookingSeat}
+        price={sumPrice}
+      />
+      
+
     </>
   );
 }

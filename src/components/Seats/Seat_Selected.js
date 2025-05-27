c6c94c67701309ac14af4e48719f20ca4d2c406c
ChaL <chaluntonvipusanapas@gmail.com>
2025-05-27 15:45:45 +0700

    feat: Update BookingCard and BookingSeats components with responsive design and seat selection icons

--- src/components/Seats/Seat_Selected.js
+++ src/components/Seats/Seat_Selected.js
@@ -1,9 +1,9 @@
 export default function SelectedIcon() {
   return (
     <svg
-      width="43"
-      height="43"
-      viewBox="0 0 43 43"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >

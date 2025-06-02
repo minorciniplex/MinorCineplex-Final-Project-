89aa86b61169e2d085ca454db1e338571e2c1b97
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-06-02 11:18:19 +0700

    refactor: Clean up payment component code and remove payment handling logic

--- src/pages/booking/seats/payment/[payment].js
+++ src/pages/booking/seats/payment/[payment].js
@@ -8,16 +8,23 @@ import useBookingSeat from "@/hooks/useBookingSeat";
 
 export default function Seats() {
   const router = useRouter();
-  const { time, screenNumber, cinemaName, poster, title, date, seat, price, showtimes, bookingId } = router.query;
+  const {
+    time,
+    screenNumber,
+    cinemaName,
+    poster,
+    title,
+    date,
+    seat,
+    price,
+    showtimes,
+    bookingId,
+  } = router.query;
   const [genres, setGenres] = useState(null);
   const [language, setLanguage] = useState(null);
   const [seatNumber, setSeatNumber] = useState(null);
-  const { processPayment,
-    isLoading,
-    error,
-    resetPaymentState } = useBookingSeat();
-
-  
+  const { processPayment, isLoading, error, resetPaymentState } =
+    useBookingSeat();
 
   useEffect(() => {
     if (router.isReady) {
@@ -25,32 +32,10 @@ export default function Seats() {
       setGenres(JSON.parse(genres));
       setLanguage(JSON.parse(language));
       setSeatNumber(JSON.parse(seat));
-
     }
   }, [router.isReady]);
 
   console.log(seatNumber, showtimes, bookingId, price);
-
-  const handlePayment = async () => {
-    const paymentData = {
-      showtimeId: showtimes,
-      seatNumber: seatNumber,
-      sumPrice: price,
-      bookingId: bookingId
-    };
-
-    const result = await processPayment(paymentData);
-
-    if (result.success) {
-      // Handle successful payment
-      console.log("Booking confirmed:", result.data);
-      // Redirect to confirmation page or update UI
-    } else {
-      // Handle payment failure
-      console.error("Payment failed:", result.error);
-    }
-  }
-
 
   return (
     <>
@@ -73,12 +58,6 @@ export default function Seats() {
             showtimes={showtimes}
           />
         </div>
-        <button className="w-full bg-[--base-gray-0] text-[--base-gray-900] p-4 rounded-lg shadow-md hover:bg-[--base-gray-100] transition-colors duration-200"
-        disabled={isLoading}
-        onClick={handlePayment}
-        >
-          payment
-        </button>
       </div>
     </>
   );
